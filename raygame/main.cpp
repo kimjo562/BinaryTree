a8fa9b7f30a4bd04cd3255440f7ba3e4bcbbfa6c
Joseph Kim <s198022@students.aie.edu>
2020-02-12 10:02:25 -0600

    Add project files.

---
+++ raygame/main.cpp
@@ -0,0 +1,53 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - basic window
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+int main()
+{
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	int screenWidth = 1278;
+	int screenHeight = 960;
+
+	InitWindow(screenWidth, screenHeight, "Zatamari");
+
+	SetTargetFPS(60);
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		// TODO: Update your variables here
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		DrawText("Skadi, Wheres Kaldr?", 440, 480, 35, WHITE);
+		DrawText("            (-.-)", 440, 580, 35, WHITE);
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------   
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+	return 0;
+}
