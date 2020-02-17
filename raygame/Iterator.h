17e13f14082664758407a973c63cffc50b0f4b38
Joseph Kim <s198022@students.aie.edu>
2020-02-17 10:07:48 -0600

    Iterators

--- raygame/Iterator.h
+++ raygame/Iterator.h
@@ -1,3 +1,4 @@
+// Iterators provide a way to access the elements of a collection without exposing its underlying representation.
 #include "Node.h"
 #pragma once
 
@@ -6,6 +7,7 @@ template <typename T>
 class Iterator
 {
 public:
+
 
 	Iterator operator++();
 	Iterator operator--();
@@ -27,40 +29,63 @@ private:
 template<typename T>
 Iterator<T> Iterator<T>::operator++()
 {
-	return Iterator();
+	// Increment iterator position
+	if (currentNode->nextNode != nullptr) currentNode = currentNode->nextNode;
+	return this;
 }
 
 template<typename T>
 Iterator<T> Iterator<T>::operator--()
 {
-	return Iterator();
+	// Decrease iterator position
+	if (currentNode->previousNode != nullptr) currentNode = currentNode->previousNode;
+	return this;
 }
 
 template<typename T>
 T Iterator<T>::operator*()
 {
+	// Dereference iterator
+	T = currentNode->info;
 	return T();
 }
 
 template<typename T>
 Iterator<T>::Iterator()
 {
+	currentNode = nullptr;
+}
+
+template<typename T>
+Iterator<T>::Iterator(Node<T>* ptr)
+{
+	currentNode = ptr;
+}
+
+template<typename T>
+bool Iterator<T>::operator==(const Iterator<T>& currentIterator) const
+{
+	if (currentNode.info == currentIterator.currentNode->info)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
 }
 
 template<typename T>
-Iterator<T>::Iterator(Node<T> * ptr)
+bool Iterator<T>::operator!=(const Iterator<T>& currentIterator) const
 {
-}
+	if (currentNode.info != currentIterator)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
-template<typename T>
-bool Iterator<T>::operator==(const Iterator<T>&) const
-{
-	return false;
-}
-
-template<typename T>
-bool Iterator<T>::operator!=(const Iterator<T>&) const
-{
-	return false;
 }
